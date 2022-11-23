91c6b9fca9cefd581cbe4b27632dde5486f62e44
Pagão <gabrielpaganinibarroso@hotmail.com>
2022-11-23 20:32:15 -0300

    author about and social medias dynamic

--- src/main/kotlin/com/blogmvc/blog/Repositories/DataLoader.kt
+++ src/main/kotlin/com/blogmvc/blog/Repositories/DataLoader.kt
@@ -46,40 +46,62 @@ class DataLoader(private val userRepository: UserRepository,
 
     private fun loadUser() {
         if (userRepository.count() == 0L) {
-            val user = User(
-                name = "Admin",
-                email = "admin@hotmail.com",
-                password = "admin"
-            )
-            userRepository.save(user).also { logger.info(it.toString()) }
+           listOf(
+               User(
+                   name = "Admin",
+                   email = "admin@hotmail.com",
+                   password = "admin"
+               ),
+               User(
+                   name = "Gabriel",
+                   email = "gabi@hotmail.com",
+                   password = "123"
+               )
+           ).also { userRepository.saveAll(it) }
         }
     }
 
     private fun loadArticle() {
         if (articleRepository.count() == 0L) {
-            val user = userRepository.findAll().get(0)
-            val author = authorRepository.save(Author(user = user))
+            val authors = authorRepository.saveAll(
+                listOf(
+                    Author(
+                        user = userRepository.findAll().get(0),
+                        about = "Sadosjadosijoadsijoadsijoadsijoadsijoads",
+                        facebook = "http://facebook.com/admin",
+                        twitter = "http://twitter.com/admin",
+                        linkedin = "http://linkedin.com/admin"
+
+                    ), Author(
+                        user = userRepository.findAll().get(1),
+                        about = "asdkadskopadskdaopskdsopdfjksgopfds",
+                        facebook = "http://facebook.com/gabriel",
+                        twitter = "http://twitter.com/gabriel",
+                        linkedin = "http://linkedin.com/gabriel"
+                    )
+                )
+            )
             listOf(
                 Article(
                     title = "Lorem ipsum dolor sit amet, consectetur adipiscing",
                     subTitle = "elit, sed do eiusmod",
                     content = "Faucibus ornare suspendisse sed nisi. Gravida quis blandit turpis cursus. Vulputate mi sit amet mauris commodo quis imperdiet massa. Risus sed vulputate odio ut enim blandit. Quis blandit turpis cursus in hac habitasse platea dictumst.",
                     date = LocalDateTime.now(),
-                    author = author
+                    author = authors.get(0)
                 ),
                 Article(
                     title = "Pharetra convallis",
                     subTitle = "pharetra vel",
                     content = "Faucibus ornare suspendisse sed nisi. Gravida quis blandit turpis cursus. Vulputate mi sit amet mauris commodo quis imperdiet massa. Risus sed vulputate odio ut enim blandit.",
                     date = LocalDateTime.now(),
-                    author = author
+                    author = authors.get(1)
                 ),
                 Article(
                     title = "convallis convallis",
                     subTitle = "Consectetur",
                     content = "Nibh tellus molestie nunc non blandit massa enim. Ipsum dolor sit amet consectetur.",
                     date = LocalDateTime.now(),
-                    author = author
+                    author = authors.get(0)
                 )
             ).also { articleRepository.saveAll(it) }
         }
